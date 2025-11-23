6bb7f7924bb9b872371c5315a485e5efe256ecf0
kirk1234630 <khurramlakhani@gmail.com>
2025-11-23 00:20:09 -0800

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,4 @@
 
-==================================================
-COMPLETE server.py
-==================================================
 
 from flask import Flask, jsonify, request
 from flask_cors import CORS
