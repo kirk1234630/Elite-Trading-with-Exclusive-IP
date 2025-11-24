b07f6a2323223d86f85e2fa18af560953e9c3bba
kirk1234630 <khurramlakhani@gmail.com>
2025-11-24 12:08:51 -0800

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -770,5 +770,5 @@ def health_check():
     }), 200
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=Fal
